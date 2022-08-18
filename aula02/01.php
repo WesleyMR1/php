929bf489260925ce1c74442c8e51d042c6221374
WesleyMR1 <WesleyMR012@gmail.com>
2022-08-17 22:08:35 -0300

    Adicionando OO na Classe Contas

--- aula02/01.php
+++ aula02/01.php
@@ -2,14 +2,9 @@
 
     include_once 'contas.php';
 
-    $conta1 = new Contas;
+    $conta1 = new Contas(123, "12345-1", "Wesley", 123456.00);
 
-    $conta1->numAge = 5028;
-    $conta1->numConta = 123456;
-    $conta1->nome = 'kayque';
-    $conta1->saldo = '587985468';
-
-    $conta1->ImprimeEtiqueta();
+    $conta1->toString();
 
 
 
--- aula02/contas.php
+++ aula02/contas.php
@@ -2,17 +2,39 @@
 
    class Contas 
    {
-       var $numAge;
-       var $numConta;
-       var $nome;
-       var $saldo;
+       private $numAge;
+       private $numConta;
+       private $nome;
+       private $saldo;
 
-       function ImprimeEtiqueta(){
-            echo 'Número agencia: ' .$this->numAge . "<br/> \n";
-            echo 'Número conta: ' .$this->numConta . "<br/> \n";
-            echo 'Nome: ' .$this->nome . "<br/> \n";
-            echo 'Saldo: ' .$this->saldo . "<br/> \n";
+       public function __construct($numAge, $numConta, $nome, $saldo){
+        $this->numAge = $numAge;
+        $this->numConta = $numConta;
+        $this->nome = $nome;
+        $this->saldo = $saldo;
+    }
+
+
+
+
+       public function toString(){
+
+            return printf("Agencia: %d Conta: %s <br> Nome: %s <br> Saldo: %.2f",$this->get_numAge(), $this->get_numConta(), $this->get_nome(), $this->get_saldo());
 
        }
+
+       public function get_numAge(){
+        return $this->numAge;
+       }
+       public function get_numConta(){
+        return $this->numConta;
+       }
+       public function get_nome(){
+        return $this->nome;
+       }
+       public function get_saldo(){
+        return $this->saldo;
+       }
+
    } 
 ?>
