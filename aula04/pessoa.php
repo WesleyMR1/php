abf9d78660e63edc2e5a52ebb7a7fe02b3ea2f71
VictorLagunaR <vitor.v.l.rlaguna@gmail.com>
2022-08-24 22:00:49 -0300

    adicionando método construtor

--- aula04/pessoa.php
+++ aula04/pessoa.php
@@ -2,16 +2,16 @@
 
     class Pessoa{
 
-        var $codigo;
-        var $nome;
-        var $altura;
-        var $idade;
-        var $nascimentos;
-        var $escolaridade;
-        var $salario;
+        private $codigo;
+        private $nome;
+        private $altura;
+        private $idade;
+        private $nascimentos;
+        private $escolaridade;
+        private $salario;
 
         public function __construct($Codigo, $nome, $altura, $idade, $nascimento, $escolaridade, $salario){
-            $this->codigo = $codigo;
+            $this->codigo = $parametrpo;
             $this->nome = $nome;
             $this->altura = $altura;
             $this->idade = $idade;
@@ -19,6 +19,10 @@
             $this->escolaridade = $escolaridade;
             $this->salario = $salario;
 
+        }
+
+        public function __destruct(){
+            echo "<br>Objeto{$this->nome} finalizado...<br>";
         }
 
         //aumenta a altura em centimetros
@@ -45,53 +49,49 @@
 
 
 
-
-
         public function get_codigo(){
             return $this->codigo;
         }
         public function set_codigo($par){
-            $this->codigo = $par:
+            $this->codigo = $par;
         }
         public function get_nome(){
             return $this->nome;
         }
         public function set_nome($par){
-            $this->nome = $par:
+            $this->nome = $par;
         }
         public function get_altura(){
             return $this->altura;
         }
         public function set_altura($par){
-            $this->altura = $par:
+            $this->altura = $par;
         }
         public function get_idade(){
             return $this->idade;
         }
         public function set_idade($par){
-            $this->idade = $par:
+            $this->idade = $par;
         }
         public function get_nascimento(){
             return $this->nascimento;
         }
         public function set_nascimento($par){
-            $this->nascimento = $par:
+            $this->nascimento = $par;
         }
         public function get_escolaridade(){
             return $this->escolaridade;
         }
         public function set_escolaridade($par){
-            $this->escolaridade = $par:
+            $this->escolaridade = $par;
         }
         public function get_salario(){
             return $this->salario;
         }
         public function set_salario($par){
-            $this->salario = $par:
+            $this->salario = $par;
         }
+        
     }
-
-    
-
 
 ?>
